28ca9340a86546259533b9555754e77f1496fbf1
techdude3000 <59034985+techdude3000@users.noreply.github.com>
2026-06-08 12:57:53 +1200

    Add function to display menu

--- Program.cs
+++ Program.cs
@@ -20,3 +20,48 @@ SHOP_ITEMS.Add("Desk Fan", 22.00);
 SHOP_ITEMS.Add("Screen Cleaner", 15.00);
 SHOP_ITEMS.Add("Power Board", 28.00);
 SHOP_ITEMS.Add("USB Adapter", 14.99);
+// Function to display the menu
+static void DisplayMenu()
+{
+    // Clear console then display a menu of difficulties and get user key input
+    Console.WriteLine("Store");
+    Console.WriteLine("""
+    [1] List all items in store
+    [2] List all items in store within budget
+    [3] View Budget
+    [4] Purchase Item
+    [5] Print all purchased items
+    [0] Exit
+    """);
+    ConsoleKeyInfo selection = Console.ReadKey(true);
+    // Exit if user presses 0
+    if (selection.Key == ConsoleKey.D0)
+    {
+    }
+    else if (selection.Key == ConsoleKey.D1)
+    {
+
+    }
+    else if (selection.Key == ConsoleKey.D2)
+    {
+
+    }
+    else if (selection.Key == ConsoleKey.D3)
+    {
+
+    }
+    else if (selection.Key == ConsoleKey.D4)
+    {
+
+    }
+    else if (selection.Key == ConsoleKey.D5)
+    {
+
+    }
+    // Clear console if user types anything else to avoid spamming console
+    else
+    {
+        Console.Clear();
+    }
+}
+DisplayMenu();
