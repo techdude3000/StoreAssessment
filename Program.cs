153bf9e83c3a60827e8ca22e7daf2b9521329fd4
techdude3000 <59034985+techdude3000@users.noreply.github.com>
2026-06-08 12:40:45 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Create dictionary to hold all of the shop items
+Dictionary<string, double> SHOP_ITEMS = new Dictionary<string, double>();
+SHOP_ITEMS.Add("Mouse", 25.00);
+SHOP_ITEMS.Add("Keyboard", 35.75);
+SHOP_ITEMS.Add("Headphones", 39.99);
+SHOP_ITEMS.Add("Webcam", 60.00);
+SHOP_ITEMS.Add("Speakers", 45.30);
+SHOP_ITEMS.Add("USB Drive", 15.00);
+SHOP_ITEMS.Add("SSD Drive", 94.99);
+SHOP_ITEMS.Add("HDMI Cable", 12.30);
+SHOP_ITEMS.Add("Ethernet Cable", 10.00);
+SHOP_ITEMS.Add("Mouse Pad", 10.00);
+SHOP_ITEMS.Add("Laptop Sleeve", 25.00);
+SHOP_ITEMS.Add("Cooling Pad", 30.00);
+SHOP_ITEMS.Add("USB Hub", 20.00);
+SHOP_ITEMS.Add("Power Bank", 49.98);
+SHOP_ITEMS.Add("SD Card", 20.00);
+SHOP_ITEMS.Add("Card Reader", 18.50);
+SHOP_ITEMS.Add("Desk Fan", 22.00);
+SHOP_ITEMS.Add("Screen Cleaner", 15.00);
+SHOP_ITEMS.Add("Power Board", 28.00);
+SHOP_ITEMS.Add("USB Adapter", 14.99);
