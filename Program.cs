692b4fa4f0f788328a351350e2509475d8f76759
techdude3000 <59034985+techdude3000@users.noreply.github.com>
2026-06-09 14:57:46 +1200

    Add function to list store items and make the budget a double

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
-﻿// Create dictionary to hold all of the shop items
-Dictionary<string, double> SHOP_ITEMS = new Dictionary<string, double>();
-SHOP_ITEMS.Add("Mouse", 25.00);
-SHOP_ITEMS.Add("Keyboard", 35.75);
-SHOP_ITEMS.Add("Headphones", 39.99);
-SHOP_ITEMS.Add("Webcam", 60.00);
-SHOP_ITEMS.Add("Speakers", 45.30);
-SHOP_ITEMS.Add("USB Drive", 15.00);
-SHOP_ITEMS.Add("SSD Drive", 94.99);
-SHOP_ITEMS.Add("HDMI Cable", 12.30);
-SHOP_ITEMS.Add("Ethernet Cable", 10.00);
-SHOP_ITEMS.Add("Mouse Pad", 10.00);
-SHOP_ITEMS.Add("Laptop Sleeve", 25.00);
-SHOP_ITEMS.Add("Cooling Pad", 30.00);
-SHOP_ITEMS.Add("USB Hub", 20.00);
-SHOP_ITEMS.Add("Power Bank", 49.98);
-SHOP_ITEMS.Add("SD Card", 20.00);
-SHOP_ITEMS.Add("Card Reader", 18.50);
-SHOP_ITEMS.Add("Desk Fan", 22.00);
-SHOP_ITEMS.Add("Screen Cleaner", 15.00);
-SHOP_ITEMS.Add("Power Board", 28.00);
-SHOP_ITEMS.Add("USB Adapter", 14.99);
+﻿using System.Linq;
+// Create dictionary to hold all of the shop items
+Dictionary<string, double> STORE_ITEMS = new Dictionary<string, double>();
+STORE_ITEMS.Add("Mouse", 25.00);
+STORE_ITEMS.Add("Keyboard", 35.75);
+STORE_ITEMS.Add("Headphones", 39.99);
+STORE_ITEMS.Add("Webcam", 60.00);
+STORE_ITEMS.Add("Speakers", 45.30);
+STORE_ITEMS.Add("USB Drive", 15.00);
+STORE_ITEMS.Add("SSD Drive", 94.99);
+STORE_ITEMS.Add("HDMI Cable", 12.30);
+STORE_ITEMS.Add("Ethernet Cable", 10.00);
+STORE_ITEMS.Add("Mouse Pad", 10.00);
+STORE_ITEMS.Add("Laptop Sleeve", 25.00);
+STORE_ITEMS.Add("Cooling Pad", 30.00);
+STORE_ITEMS.Add("USB Hub", 20.00);
+STORE_ITEMS.Add("Power Bank", 49.98);
+STORE_ITEMS.Add("SD Card", 20.00);
+STORE_ITEMS.Add("Card Reader", 18.50);
+STORE_ITEMS.Add("Desk Fan", 22.00);
+STORE_ITEMS.Add("Screen Cleaner", 15.00);
+STORE_ITEMS.Add("Power Board", 28.00);
+STORE_ITEMS.Add("USB Adapter", 14.99);
 // Define budget variable
-int budget = 0;
+double budget = 0;
 // Method that will get the users input and test if it has no errors
-static int GetUserInput()
+static double GetUserInput()
 {
     while (true)
     {
@@ -34,15 +35,48 @@ static int GetUserInput()
         {
             Console.WriteLine("Input can not be empty");
         }
-        // Tell user if string is cannot be converted to an integer
-        else if (!int.TryParse(input, out int inputInt))
+        // Tell user if string is cannot be converted to an double
+        else if (!double.TryParse(input, out double inputDouble))
         {
-            Console.WriteLine("Input must be a valid integer (whole number).");
+            Console.WriteLine("Input must be a valid number (float).");
         }
-        // Return if it passes the tests    
+        // Return if it passes the tests
         else
         {
-            return inputInt;
+            return inputDouble;
+        }
+    }
+}
+static void ListStoreItems(Dictionary<string, double> storeItems, double? budget)
+{
+    if (budget.HasValue)
+    {
+        List<string> itemsInBudget = new List<string>{};
+        foreach (var i in storeItems)
+        {
+            if (i.Value <= budget)
+            {
+                itemsInBudget.Add($"{i.Key}: ${i.Value}");
+            }
+        }
+        if (itemsInBudget.Count > 0)
+        {
+            foreach (string i in itemsInBudget)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"There are no items in the store within your budget of ${budget}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("These are all the items in the store:");
+        foreach (var i in storeItems)
+        {
+            Console.WriteLine($"{i.Key}: ${i.Value}");
         }
     }
 }
@@ -66,11 +100,11 @@ void DisplayMenu()
     }
     else if (selection.Key == ConsoleKey.D1)
     {
-
+        ListStoreItems(STORE_ITEMS, null);
     }
     else if (selection.Key == ConsoleKey.D2)
     {
-
+        ListStoreItems(STORE_ITEMS, budget);
     }
     else if (selection.Key == ConsoleKey.D3)
     {
